26b5ea45bbdbf4b2a9e70daced42260fb852c93d
snoo <wm28744@gmail.com>
2023-04-19 14:27:36 -0500

    poop

--- src/Patient.java
+++ src/Patient.java
@@ -2,6 +2,7 @@ import desmoj.core.simulator.*;
 public class Patient extends Entity{
     //Entity attributes
     protected boolean referred;
+    //this is a test
 
 
     public Patient(Model owner, String name, boolean showInTrace) {

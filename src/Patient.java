c763b7525e936c241c8bdd51edef9189ac405d80
snoo <wm28744@gmail.com>
2023-04-24 14:56:38 -0500

    this is DUMB

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,4 @@
+import desmoj.core.dist.BoolDistBernoulli;
 import desmoj.core.simulator.*;
 import co.paralleluniverse.fibers.SuspendExecution;
 import java.util.concurrent.TimeUnit;
@@ -12,6 +13,57 @@ public class Patient extends SimProcess{
         medicalModel model = (medicalModel)getModel();
         //get starting time
         double startTime = model.presentTime().getTimeAsDouble(TimeUnit.MINUTES);
+        //get size of waiting room
+        int waitingRoomSize = model.waitingRoom.size();
+        BoolDistBernoulli balks = new BoolDistBernoulli(model, "Balks", (double) waitingRoomSize/8, true, true);
+        //check if patient balks
+        boolean balk = balks.sample();
+        if(balk){
+            //patient balks
+            model.patientsBalk.update();
+            model.sendTraceNote(this.getName() + " has balked");
+        } else {
+            //patient does not balk
+            //enter waiting room
+            model.waitingRoom.insert(this);
+            model.sendTraceNote(this.getName() + " has entered the waiting room");
+            if(model.nurseIsBusy){
+                passivate();
+            } else {
+                model.nurseIsBusy = true;
+                model.waitingRoom.removeFirst();
+                double treatmentTime = model.treatmentTime.sample();
+                hold(new TimeSpan(treatmentTime, TimeUnit.MINUTES));
+                model.nurseIsBusy = false;
+                //check if patient gets referred to specialist
+                boolean referral = model.referralRate.sample();
+                if(!referral){
+                    //patient is fully treated
+                    model.sendTraceNote(this.getName() + " has been treated");
+                    model.patientsTreated.update();
+                    model.responseTimeFullyTreated.update(model.presentTime().getTimeAsDouble(TimeUnit.MINUTES) - startTime);
+                } else {
+                    //patient is referred to specialist
+                    model.sendTraceNote(this.getName() + " has been referred to a specialist");
+                    model.specialistQueue.insert(this);
+                    if(model.specialistIsBusy){
+                        passivate();
+                    } else {
+                        model.specialistIsBusy = true;
+                        model.specialistQueue.removeFirst();
+                        double specialistTreatmentTime = model.specialistTreatmentTime.sample();
+                        hold(new TimeSpan(specialistTreatmentTime, TimeUnit.MINUTES));
+                        model.specialistIsBusy = false;
+                        model.sendTraceNote(this.getName() + " has been treated");
+                        model.patientsTreated.update();
+                        model.responseTimeFullyTreated.update(model.presentTime().getTimeAsDouble(TimeUnit.MINUTES) - startTime);
+                    }
+                }
+
+            }
+
+        }
+
 
 
 
